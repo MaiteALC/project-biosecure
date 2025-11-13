097ca6147c64d893969a4959fe8d4dd8be985814
MaiteALC <maitexp549@gmail.com>
2025-11-13 20:28:05 -0300

    Removed client factory (no more necessary)

--- apps/web-api-java/src/main/java/br/com/biosecure/domain/client/Client.java
+++ apps/web-api-java/src/main/java/br/com/biosecure/domain/client/Client.java
@@ -1,7 +1,8 @@
 package br.com.biosecure.domain.client;
 
 import java.util.UUID;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import br.com.biosecure.domain.client.Adress;
 import br.com.biosecure.domain.client.Cnpj;
 
@@ -13,9 +14,17 @@ public class  Client {
     private String email;
     
     Client(String corporateName, UUID id, Cnpj cnpj, Adress adress, String email) {
+        if (corporateName == null || corporateName.equals("")) {
+            throw new IllegalArgumentException("Please enter corporate name");
+        }
+        
+        if (!validateEmail(email)) {
+            throw new IllegalArgumentException("Please enter a valid corporate email");
+        }
+
         this.corporateName = corporateName;
         this.cnpj = cnpj;
-        this.id = id;
+        this.id = UUID.randomUUID();
         this.adress = adress;
         this.email = email;
     } 
@@ -56,6 +65,20 @@ public class  Client {
         String str = "Corporate Name: " + corporateName + "\nCNPJ: " + cnpj.getFormattedNumber() + "\nID: " + id + "\nAdress: " + adress + "\nEmail: " + email;
 
         return str;
+    }
+
+    private boolean validateEmail(String email) {
+        if (email == null || email.equals("")) {
+            return false;
+        }
+
+        final String REGEX = "^[A-Za-z0-9._%+-]+@(?!(gmail\\.com|hotmail\\.com|outlook\\.com|yahoo\\.com|live\\.com)$)[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+
+        Pattern pattern = Pattern.compile(REGEX, Pattern.CASE_INSENSITIVE);
+
+        Matcher matcher = pattern.matcher(email);
+
+        return matcher.matches();
     }
 
     public String getCorporateName() {
