3760fe1d3942da22baf5fb9d04f2368c426e80c3
MaiteALC <maitexp549@gmail.com>
2025-11-23 21:13:49 -0300

    Added some new fields to the class Sanitizer, implemented the ChemicalBase enum and validation rules in constructor and changed PhysicalForm codes

--- apps/web-api-java/src/main/java/br/com/biosecure/model/product/Sanitizer.java
+++ apps/web-api-java/src/main/java/br/com/biosecure/model/product/Sanitizer.java
@@ -1,42 +1,121 @@
 package br.com.biosecure.model.product;
 
+import java.util.ArrayList;
 import java.time.LocalDate;
 
 public class Sanitizer extends Product {
-    private final String activeIngredient;
+    private final ChemicalBase activeIngredient;
     private final PhysicalForm form;
     private final String registerNumber;
-    private String useIndications;
+    private final String useIndications;
+    private final double phLevel;
+    private final double concentration;
+    private final ConcentrationUnit concentrationUnit;
+    private final boolean flammable;
+    private final boolean requiresDilution;
 
-    public Sanitizer(String name, double price, String activeIngredient, PhysicalForm form, String manufacturer, String batchNumber, LocalDate expirationDate, PackagingType packagingType, MeasureUnity measureUnity, int qtdPerPackage, String registerNumber, String useIndications) {
-        super(name, price, manufacturer, batchNumber, expirationDate, packagingType, measureUnity, qtdPerPackage);
+
+    public Sanitizer(String name, double price, ChemicalBase activeIngredient, PhysicalForm form, String manufacturer, String batchNumber, LocalDate expirationDate, PackagingType packagingType, MeasureUnit measureUnit, int qtdPerPackage, String registerNumber, String useIndications, double phLevel, boolean isFlammable, double concentration, ConcentrationUnit concentrationUnit, boolean requiresDilution) {
+        super(name, price, manufacturer, batchNumber, expirationDate, packagingType, measureUnit, qtdPerPackage);
+
+        if (phLevel < 0 || phLevel > 14) {
+            throw new InvalidProductAttributeException("ph level");
+        }
+
+        if (concentration < 0 || (concentrationUnit == ConcentrationUnit.PERCENTAGE && concentration > 100) || (concentrationUnit == ConcentrationUnit.PPM && concentration > 1000000) ){
+            throw new InvalidProductAttributeException("concentration");
+        }
+
+        validateBioSafetyRules(activeIngredient, isFlammable, phLevel);
 
         this.activeIngredient = activeIngredient;
         this.form = form;
         this.registerNumber = registerNumber;
         this.useIndications = useIndications;
+        this.phLevel = phLevel;
+        this.flammable = isFlammable;
+        this.concentration = concentration;
+        this.concentrationUnit = concentrationUnit;
+        this.requiresDilution = requiresDilution;
+    }
+
+    public enum ChemicalBase {
+        ETHANOL,
+        ALCOHOL_ISOPROPYL,
+        SODIUM_HYPOCHLORITE,
+        QUATERNARY_AMMONIUM,
+        PERACETIC_ACID,
+        ENZYMATIC_DETERGENT,
+        CHLORHEXIDINE
     }
 
     public enum PhysicalForm {
-        LIQUID(1),
-        SOLID(2),
-        POWDER(3),
-        GEL(4),
-        SPRAY(5),
-        GASEOUS(6);
+        LIQUID("LQ"),
+        SOLID("SO"),
+        POWDER("PD"),
+        GEL("GL"),
+        SPRAY("SP"),
+        FOAM("FM"),
+        WIPES("WP");
 
-        private final int code;
+        private final String code;
 
-        private PhysicalForm(int code) {
+        PhysicalForm(String code) {
             this.code = code;
         }
 
-        public int getPhysicalFormCode() {
+        public String getCode() {
             return code;
         }
     }
 
-    public String getActiveIngredient() {
+    public enum ConcentrationUnit {
+        PERCENTAGE("%"),
+        PPM("ppm"),
+        GL("°GL"),
+        MG_L("mg/L");
+
+        private final String symbol;
+
+        ConcentrationUnit(String symbol) {
+            this.symbol = symbol;
+        }
+
+        public String getSymbol() {
+            return symbol;
+        }
+    }
+
+    private void validateBioSafetyRules(ChemicalBase chemicalBase, boolean isFlammable, double phLevel) {
+        ArrayList<String> invalids = new ArrayList<>();
+        invalids.add("Active ingredient");
+
+        if ((chemicalBase == ChemicalBase.ETHANOL || chemicalBase == ChemicalBase.ALCOHOL_ISOPROPYL) && this.concentration > 40 && !isFlammable) {
+            invalids.add("Concentration");
+
+            throw new BioSecurityException(
+                    "Alcohols with 40% (or more concentration) MUST be flammable.", invalids
+            );
+        }
+
+        if (chemicalBase == ChemicalBase.SODIUM_HYPOCHLORITE && phLevel < 8.0) {
+            invalids.add("Ph level");
+
+            throw new BioSecurityException(
+                    "Sodium hypochlorite must be alkaline pH level (>8). Acid pH makes toxic gas.", invalids
+            );
+        }
+
+        if (chemicalBase == ChemicalBase.ENZYMATIC_DETERGENT && (phLevel < 5 || phLevel > 9)) {
+            invalids.add("Ph level");
+
+            throw new BioSecurityException(
+                    "Enzymatic detergents requires neutral pH to work correctly.", invalids
+            );
+        }
+    }
+
+    public ChemicalBase getActiveIngredient() {
         return activeIngredient;
     }
 
@@ -52,11 +131,23 @@ public class Sanitizer extends Product {
         return useIndications;
     }
 
-    public void setUseIndications(String newUseIndications) {
-        if (newUseIndications.isBlank()) {
-            throw new InvalidProductAttributeException("use indications");
-        }
+    public double getPhLevel() {
+        return phLevel;
+    }
 
-        this.useIndications = newUseIndications;
+    public double getConcentration() {
+        return concentration;
+    }
+
+    public ConcentrationUnit getConcentrationUnit() {
+        return concentrationUnit;
+    }
+
+    public boolean isFlammable() {
+        return flammable;
+    }
+
+    public boolean isRequiresDilution() {
+        return requiresDilution;
     }
 }
