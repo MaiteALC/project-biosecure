109127f4bf268837badb7da225a8223c0db3b77d
MaiteALC <maitexp549@gmail.com>
2026-02-15 16:10:06 -0300

    refactor(Partner): remove jackson annotations
    Remove all annotations used to map JSON properties to record fields. A dedicated DTO will handle JSON mapping.

--- apps/customers-api/src/main/java/br/com/biosecure/model/Partner.java
+++ apps/customers-api/src/main/java/br/com/biosecure/model/Partner.java
@@ -1,16 +1,13 @@
 package br.com.biosecure.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.LocalDate;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
 public record Partner (
-    @JsonProperty("identificador_de_socio") String partnerIdentifier,
-    @JsonProperty("nome_socio") String partnerName,
-    @JsonProperty("cnpj_cpf_do_socio") String partnerCpfOrCnpj,
-    @JsonProperty("codigo_qualificacao_socio") String partnerTypeCode,
-    @JsonProperty("qualificacao_socio") String partnerType,
-    @JsonProperty("data_entrada_sociedade") LocalDate entryDate,
-    @JsonProperty("codigo_faixa_etaria") int ageRangeCode
+        String partnerIdentifier,
+        String partnerName,
+        String partnerCpfOrCnpj,
+        String partnerTypeCode,
+        String partnerType,
+        LocalDate entryDate,
+        int ageRangeCode
 ) {}
