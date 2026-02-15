cdef60c7c8539a05d7d93811256199e20300f150
MaiteALC <maitexp549@gmail.com>
2026-02-15 17:10:13 -0300

    refactor(MinhaReceitaResponse): use DTOs to transport cnae and partner data
    The 'secondaryCnaeNumbers' and 'partnerList' attributes now use their respective external DTOs instead of domain entities to transport data.

--- apps/customers-api/src/main/java/br/com/biosecure/external/MinhaReceitaResponse.java
+++ apps/customers-api/src/main/java/br/com/biosecure/external/MinhaReceitaResponse.java
@@ -1,8 +1,6 @@
 package br.com.biosecure.external;
 
-import br.com.biosecure.model.Cnae;
 import br.com.biosecure.model.Cnpj;
-import br.com.biosecure.model.Partner;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.LocalDate;
@@ -10,16 +8,38 @@ import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record MinhaReceitaResponse(
+
         Cnpj cnpj,
-        @JsonProperty("descricao_situacao_cadastral") String registrationStatusDescription,
-        @JsonProperty("data_situacao_cadastral") LocalDate registrationStatusDate,
-        @JsonProperty("data_inicio_atividade") LocalDate startDateActivities,
-        @JsonProperty("cnae_fiscal") String cnaeNumber,
-        @JsonProperty("cnae_fiscal_descricao") String cnaeDescription,
+
+        @JsonProperty("descricao_situacao_cadastral")
+        String registrationStatusDescription,
+
+        @JsonProperty("data_situacao_cadastral")
+        LocalDate registrationStatusDate,
+
+        @JsonProperty("data_inicio_atividade")
+        LocalDate startDateActivities,
+
+        @JsonProperty("cnae_fiscal")
+        String cnaeNumber,
+
+        @JsonProperty("cnae_fiscal_descricao")
+        String cnaeDescription,
+
         String cep,
-        @JsonProperty("razao_social") String corporateName,
-        @JsonProperty("natureza_juridica") String legalNature,
-        @JsonProperty("capital_social") long shareCapital,
-        @JsonProperty("qsa") List<Partner> partnersList,
-        @JsonProperty("cnaes_secundarios") List<Cnae> secondaryCnaeNumbers
+
+        @JsonProperty("razao_social")
+        String corporateName,
+
+        @JsonProperty("natureza_juridica")
+        String legalNature,
+
+        @JsonProperty("capital_social")
+        long shareCapital,
+
+        @JsonProperty("qsa")
+        List<PartnerExternalDto> partnersList,
+
+        @JsonProperty("cnaes_secundarios")
+        List<CnaeExternalDto> secondaryCnaeNumbers
 ) {}
