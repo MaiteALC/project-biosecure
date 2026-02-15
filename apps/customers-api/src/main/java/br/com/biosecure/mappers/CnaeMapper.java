aacda575851b6d509ed547a4b7d566830afc6b24
MaiteALC <maitexp549@gmail.com>
2026-02-15 17:00:40 -0300

    feat(Cnae): introduce cnae mapper
    - Add mapper class to perform DTO-Entity conversion
    - Add unit tests ensuring mapping accuracy and null handling

---
+++ apps/customers-api/src/main/java/br/com/biosecure/mappers/CnaeMapper.java
@@ -0,0 +1,24 @@
+package br.com.biosecure.mappers;
+
+import br.com.biosecure.dto.CnaeInputDto;
+import br.com.biosecure.dto.CnaeResponseDto;
+import br.com.biosecure.model.Cnae;
+
+public class CnaeMapper {
+
+    public static Cnae toEntity(CnaeInputDto dto) {
+        if (dto == null) {
+            throw new NullPointerException("A CNAE DTO is required");
+        }
+
+        return new Cnae(dto.code(),  dto.description());
+    }
+
+    public static CnaeResponseDto toDto(Cnae entity) {
+        if (entity == null) {
+            throw new NullPointerException("A CNAE entity is required");
+        }
+
+        return new CnaeResponseDto(entity.getFormattedCode(), entity.getDescription());
+    }
+}
---
+++ apps/customers-api/src/test/java/br/com/biosecure/mappers/CnaeMapperTest.java
@@ -0,0 +1,41 @@
+package br.com.biosecure.mappers;
+
+import br.com.biosecure.dto.CnaeInputDto;
+import br.com.biosecure.dto.CnaeResponseDto;
+import br.com.biosecure.model.Cnae;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class CnaeMapperTest {
+
+    @Test
+    void shouldConvertToEntityCorrectly() {
+        CnaeInputDto dto = new CnaeInputDto("8630-5/02", "test description");
+
+        Cnae cnae = CnaeMapper.toEntity(dto);
+
+        assertEquals(dto.code(), cnae.getFormattedCode());
+        assertEquals("8630502", cnae.getUnformattedCode());
+    }
+
+    @Test
+    void shouldConvertToDtoCorrectly() {
+        Cnae cnae = new Cnae("8630-5/02",  "test description");
+
+        CnaeResponseDto dto = CnaeMapper.toDto(cnae);
+
+        assertEquals(cnae.getFormattedCode(), dto.code());
+    }
+
+    @Test
+    void shouldThrowException_WhenInputIsNull() {
+        NullPointerException exception = assertThrows(NullPointerException.class, () -> CnaeMapper.toDto(null));
+
+        NullPointerException exception2 = assertThrows(NullPointerException.class, () -> CnaeMapper.toEntity(null));
+
+        assertEquals("A CNAE entity is required", exception.getMessage());
+        assertEquals("A CNAE DTO is required", exception2.getMessage());
+    }
+}
