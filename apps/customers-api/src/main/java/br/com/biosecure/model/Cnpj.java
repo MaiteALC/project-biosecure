7b790f5b873d480c59d6b64604bbdcff998c5802
MaiteALC <maitexp549@gmail.com>
2026-02-15 17:49:02 -0300

    style(Cnpj): rename constructor parameter
    Rename parameter from 'unformattedNumber' to 'string' to avoid confusion about what can be used to instantiate a Cnpj.

--- apps/customers-api/src/main/java/br/com/biosecure/model/Cnpj.java
+++ apps/customers-api/src/main/java/br/com/biosecure/model/Cnpj.java
@@ -18,16 +18,16 @@ public class Cnpj {
     private static final int SIZE_UNFORMATTED = 14;
     private static final Pattern CNPJ_REGEX = Pattern.compile("^[0-9]{2}\\.?[0-9]{3}\\.?[0-9]{3}/?[0-9]{4}-?[0-9]{2}$");
 
-    public Cnpj(String unformattedNumber) {
-        if (unformattedNumber == null || unformattedNumber.isBlank()) {
+    public Cnpj(String string) {
+        if (string == null || string.isBlank()) {
             throw new InvalidCnpjException("CNPJ number is null/blank");
         }
 
-        if (!CNPJ_REGEX.matcher(unformattedNumber).matches()) {
+        if (!CNPJ_REGEX.matcher(string).matches()) {
             throw new InvalidCnpjException("CNPJ with invalid format");
         }
 
-        String cleanNumber = clearFormat(unformattedNumber);
+        String cleanNumber = clearFormat(string);
 
         validateVerifierDigits(cleanNumber);
 
