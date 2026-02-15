019f027347d9ed58194a85447c183ba67fbc92b6
MaiteALC <maitexp549@gmail.com>
2026-02-15 16:55:11 -0300

    feat(Partner): introduce partner mapper
    - Add mapper class to perform DTO-Entity conversion
    - Add unit tests ensuring mapping accuracy and null handling

---
+++ apps/customers-api/src/main/java/br/com/biosecure/external/PartnerMapper.java
@@ -0,0 +1,23 @@
+package br.com.biosecure.external;
+
+import br.com.biosecure.dto.PartnerResponseDto;
+import br.com.biosecure.model.Partner;
+
+public class PartnerMapper {
+
+    public static Partner toEntity(PartnerExternalDto dto) {
+        if (dto == null) {
+            throw new NullPointerException("A partner DTO is required");
+        }
+
+        return new Partner(dto.identifierCode(), dto.name(), dto.cpfOrCnpj(), dto.typeCode(), dto.type(), dto.entryDate(), dto.ageRangeCode());
+    }
+
+    public static PartnerResponseDto toDto(Partner entity) {
+        if (entity == null) {
+            throw new NullPointerException("A partner entity is required");
+        }
+
+        return new PartnerResponseDto(entity.identifierCode(), entity.name(), entity.type(), entity.entryDate());
+    }
+}
---
+++ apps/customers-api/src/test/java/br/com/biosecure/external/PartnerMapperTest.java
@@ -0,0 +1,43 @@
+package br.com.biosecure.external;
+
+import br.com.biosecure.dto.PartnerResponseDto;
+import br.com.biosecure.model.Partner;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class PartnerMapperTest {
+
+    @Test
+    void shouldConvertToPartnerDtoCorrectly() {
+        Partner partner = new Partner(2, "Test Name", "***123***", 10, "Diretor", LocalDate.now().minusYears(2), 5);
+
+        PartnerResponseDto partnerResponseDto = PartnerMapper.toDto(partner);
+
+        assertEquals(partner.name(), partnerResponseDto.name());
+        assertEquals(partner.type(), partnerResponseDto.type());
+    }
+
+    @Test
+    void shouldConvertToPartnerEntityCorrectly() {
+        PartnerExternalDto dto = new PartnerExternalDto(2, "Test Name", "***456***", 9, "Socio", LocalDate.now().minusYears(2), 4);
+
+        Partner partner = PartnerMapper.toEntity(dto);
+
+        assertEquals(partner.name(), dto.name());
+        assertEquals(partner.type(), dto.type());
+    }
+
+    @Test
+    void shouldThrowException_WhenInputIsNull() {
+        NullPointerException exception = assertThrows(NullPointerException.class, () -> PartnerMapper.toDto(null));
+
+        NullPointerException exception2 = assertThrows(NullPointerException.class, () -> PartnerMapper.toEntity(null));
+
+        assertEquals("A partner entity is required", exception.getMessage());
+        assertEquals("A partner DTO is required", exception2.getMessage());
+    }
+}
