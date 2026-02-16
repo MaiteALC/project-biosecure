249549ee89c1eca36cfdc6f481bfaa5a92dd4d66
MaiteALC <maitexp549@gmail.com>
2026-02-16 08:03:07 -0300

    feat(CnaeExternalDto): add jackson annotations
    Use annotations to map JSON properties to their respective fields in the DTO.

--- apps/customers-api/src/main/java/br/com/biosecure/external/CnaeExternalDto.java
+++ apps/customers-api/src/main/java/br/com/biosecure/external/CnaeExternalDto.java
@@ -1,11 +1,15 @@
 package br.com.biosecure.external;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 
 public record CnaeExternalDto(
+
+        @JsonProperty(value = "codigo")
         @NotBlank(message = "CNAE number is required")
         String code,
 
+        @JsonProperty(value = "descricao")
         @NotBlank(message = "CNAE description is required")
         String description
 ) {}
