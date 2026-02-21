9415319cdfc77e3fa1b74e7ea69025eba15872fa
MaiteALC <maitexp549@gmail.com>
2026-02-21 13:57:43 -0300

    refactor(CustomerResponseDto): remove not null validations
    Remove validations from customer related entities to allow scenarios where the API client doesn't want the full data.
    Change validation on 'cnpj' field to NotBlank

--- apps/customers-api/src/main/java/br/com/biosecure/dto/response/CustomerResponseDto.java
+++ apps/customers-api/src/main/java/br/com/biosecure/dto/response/CustomerResponseDto.java
@@ -2,7 +2,6 @@ package br.com.biosecure.dto.response;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 import java.util.Set;
@@ -12,22 +11,19 @@ public record CustomerResponseDto (
         @NotBlank(message = "Corporate name is required")
         String corporateName,
 
-        @NotNull(message = "CNPJ number is required")
+        @NotBlank(message = "CNPJ number is required")
         String cnpj,
 
         @NotBlank(message = "Email is required")
         String email,
 
         @Valid
-        @NotNull
         Set<AddressResponseDto> addresses,
 
         @Valid
-        @NotNull
         FinancialDataResponseDto financialData,
 
         @Valid
-        @NotNull
         List<TaxDataResponseDto> taxData
 
 ) implements CustomerDto {}
